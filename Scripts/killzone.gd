e2155ed76594b156a6a818ed54e8bea3de8e3cfe
Zenxtho <kgadityagupta@gmail.com>
2026-07-12 10:18:49 +0530

    Update killzone.gd

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -5,11 +5,14 @@ extends Area2D
 @onready var audio_stream_player: AudioStreamPlayer = $"../../../AudioStreamPlayer"
 
 func _on_body_entered(body: Node2D) -> void:
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	print("u died")
 	audio_stream_player_2.play()
 	audio_stream_player.stop()
 	timer.start()
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
 	
